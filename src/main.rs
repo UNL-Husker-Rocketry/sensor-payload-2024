4ff2e523def9e80a72f49f496b85e3e7ebe08955
G2-Games <ke0bhogsg@gmail.com>
2024-02-08 00:42:22 -0600

    Initial LoRa transmitting implemented

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,11 @@ use embassy_executor::Spawner;
 use embassy_rp::{
     bind_interrupts,
     gpio::{self, AnyPin},
+    spi::{self, Spi},
     peripherals::USB,
     usb::{Driver, InterruptHandler},
 };
-use embassy_time::Timer;
+use embassy_time::{Timer, Delay};
 use gpio::{Level, Output};
 use log::info;
 use {defmt_rtt as _, panic_probe as _};
@@ -22,25 +23,81 @@ async fn logger_task(driver: Driver<'static, USB>) {
     embassy_usb_logger::run!(1024, log::LevelFilter::Info, driver);
 }
 
+/// Set up a task to blink the LED
+#[embassy_executor::task]
+async fn blink_led(mut led: Output<'static, AnyPin>) {
+    loop {
+        led.set_high();
+        Timer::after_secs(1).await;
+
+        led.set_low();
+        Timer::after_secs(1).await;
+    }
+}
+
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
 
-    // Set up USB logging
+    // Set up USB serial logging
     let driver = Driver::new(p.USB, Irqs);
     spawner.spawn(logger_task(driver)).unwrap();
 
-    Timer::after_secs(1).await;
+    // Wait for a bit for everything to start up, this
+    // along with the serial logging should be removed
+    // before being used for real
+    Timer::after_secs(2).await;
 
-    let mut led = Output::new(AnyPin::from(p.PIN_13), Level::Low);
+    let led = Output::new(AnyPin::from(p.PIN_13), Level::Low);
+    spawner.spawn(blink_led(led)).unwrap();
+
+    // Set up all the pins needed for the LoRa module
+    // Documentation here: https://learn.adafruit.com/feather-rp2040-rfm95/pinouts
+    // And here: https://github.com/mr-glt/sx127x_lora
+    let miso = p.PIN_8;
+    let mosi = p.PIN_15;
+    let clk = p.PIN_14;
+    let rfm_cs = p.PIN_16; // Chip Select
+    let rfm_rst = p.PIN_17; // Reset
+
+    // Set up the SPI interface
+    let mut config = spi::Config::default();
+    config.frequency = 20_000;
+    let spi = Spi::new_blocking(p.SPI1, clk, mosi, miso, config);
+
+    // Set up Chip Select and Reset
+    let cs = Output::new(rfm_cs, Level::Low);
+    let reset = Output::new(rfm_rst, Level::Low);
+
+    // Actually initialize the LoRa module
+    let mut lora = match sx127x_lora::LoRa::new(
+        spi, cs, reset, 915, Delay
+    ) {
+        Ok(lora) => {
+            info!("Success initing module!");
+            lora
+        },
+        Err(err) => {
+            info!("Error initalizing: {:?}", err);
+            panic!();
+        }
+    };
+
+    lora.set_tx_power(17, 1).expect("Could not set power");
+
+    let message = "Hello, world!";
+    let mut buffer = [0; 255];
+    for (i, c) in message.chars().enumerate() {
+        buffer[i] = c as u8
+    }
 
     loop {
-        info!("Test");
+        let transmit = lora.transmit_payload(buffer, message.len());
+        match transmit {
+            Ok(_) => info!("Transmit success!"),
+            Err(_) => info!("Couldn't transmit!"),
+        }
 
-        led.set_high();
-        Timer::after_millis(100).await;
-
-        led.set_low();
-        Timer::after_millis(100).await;
+        Timer::after_millis(1000).await;
     }
 }
